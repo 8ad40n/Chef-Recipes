7c53cd2e440f3d223fa8a3b2df528aa185c02a07
8ad40n <joy184110@gmail.com>
2024-06-25 20:37:17 +0600

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/header/header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      APP
+    <Header></Header>
     </>
   )
 }
